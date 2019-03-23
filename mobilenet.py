98631b058b21c758870e9a9d793086ba589166de
Pedro Prates <phprates@me.com>
2019-03-23 11:10:38 -0300

    adding width multiplier parameter alpha

--- mobilenet.py
+++ mobilenet.py
@@ -6,22 +6,25 @@ class MobileNetV2(nn.Module):
     def __init__(self, t, p, output_channels=1000, alpha=1, rho=1):
         super(MobileNetV2, self).__init__()
 
-        self.initial_conv = nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1)
+        self.channels = [32, 16, 24, 32, 64, 96, 160, 320] * alpha
+
+        self.initial_conv = nn.Conv2d(3, self.channels[0], kernel_size=3, stride=2, padding=1)
         self.initial_bn = nn.BatchNorm2d(32)
 
-        self.layer1 = self._make_layer('bottleneck_1', 1, 32, 16, 1, 1)
-        self.layer2 = self._make_layer('bottleneck_2', t, 16, 24, 2, 2)
-        self.layer3 = self._make_layer('bottleneck_3', t, 24, 32, 3, 2)
-        self.layer4 = self._make_layer('bottleneck_4', t, 32, 64, 4, 2)
-        self.layer5 = self._make_layer('bottleneck_5', t, 64, 96, 3, 1)
-        self.layer6 = self._make_layer('bottleneck_6', t, 96, 160, 3, 2)
-        self.layer7 = self._make_layer('bottleneck_7', t, 160, 320, 1, 1)
+        self.layer1 = self._make_layer('bottleneck_1', 1, self.channels[0], self.channels[1], 1, 1)
+        self.layer2 = self._make_layer('bottleneck_2', t, self.channels[1], self.channels[2], 2, 2)
+        self.layer3 = self._make_layer('bottleneck_3', t, self.channels[2], self.channels[3], 3, 2)
+        self.layer4 = self._make_layer('bottleneck_4', t, self.channels[3], self.channels[4], 4, 2)
+        self.layer5 = self._make_layer('bottleneck_5', t, self.channels[4], self.channels[5], 3, 1)
+        self.layer6 = self._make_layer('bottleneck_6', t, self.channels[5], self.channels[6], 3, 2)
+        self.layer7 = self._make_layer('bottleneck_7', t, self.channels[6], self.channels[7], 1, 1)
 
         self.pointwise1 = nn.Conv2d(320, 1280, kernel_size=1, stride=1, padding=1)
         self.bn2 = nn.BatchNorm2d(1280)
 
         self.avg_pool = nn.AvgPool2d((7, 7))
         self.final_pointwise = nn.Conv2d(1280, output_channels, kernel_size=1)
+        self.dropout = nn.Dropout(p)
 
         self.relu = nn.ReLU6()
 
@@ -53,6 +56,7 @@ class MobileNetV2(nn.Module):
         x = self.avg_pool(x)
 
         x = self.final_pointwise(x)
+        x = self.dropout(x)
         x = self.relu(x)
 
         return x
